b64e69bbfae25e440548d98640cb07b5926889ce
Francesco Piantini <francesco.piantini@gmail.com>
2020-11-21 18:08:46 +0100

    added chess related bitboard state values; added test for initial position of chess board

--- include/bitboard/bitboard.h
+++ include/bitboard/bitboard.h
@@ -20,7 +20,37 @@ namespace bitboard
   const BitBoardState Corners                      = 0x8100000000000081;
 
   // Chess related
-  const BitBoardState ChTwoKingsInitial            = 0x1000000000000010;
+  const BitBoardState CH_WhiteKingInitial          = 0x0000000000000010;
+  const BitBoardState CH_BlackKingInitial          = 0x1000000000000000;
+  const BitBoardState CH_WhiteQueenInitial         = 0x0000000000000008;
+  const BitBoardState CH_BlackQueenInitial         = 0x0800000000000000;
+  const BitBoardState CH_WhiteBishopsInitial       = 0x0000000000000042;
+  const BitBoardState CH_BlackBishopsInitial       = 0x4200000000000000;
+  const BitBoardState CH_WhiteKnightsInitial       = 0x0000000000000024;
+  const BitBoardState CH_BlackKnightsInitial       = 0x2400000000000000;
+  const BitBoardState CH_WhiteRooksInitial         = 0x0000000000000081;
+  const BitBoardState CH_BlackRooksInitial         = 0x8100000000000000;
+  const BitBoardState CH_WhitePawnsInitial         = 0x000000000000ff00;
+  const BitBoardState CH_BlackPawnsInitial         = 0x00ff000000000000;
+  const BitBoardState CH_BothKingsInitial          = CH_WhiteKingInitial | CH_BlackKingInitial;
+  const BitBoardState CH_BothQueensInitial         = CH_WhiteQueenInitial | CH_BlackQueenInitial;
+  const BitBoardState CH_BothBishopsInitial        = CH_WhiteBishopsInitial | CH_BlackBishopsInitial;
+  const BitBoardState CH_BothKnightsInitial        = CH_WhiteKnightsInitial | CH_BlackKnightsInitial;
+  const BitBoardState CH_BothRooksInitial          = CH_WhiteRooksInitial | CH_BlackRooksInitial;
+  const BitBoardState CH_BothPawnsInitial          = CH_WhitePawnsInitial | CH_BlackPawnsInitial;
+  const BitBoardState CH_WhiteFullInitial          = CH_WhiteKingInitial |
+                                                     CH_WhiteQueenInitial |
+                                                     CH_WhiteBishopsInitial |
+                                                     CH_WhiteKnightsInitial |
+                                                     CH_WhiteRooksInitial |
+                                                     CH_WhitePawnsInitial;
+  const BitBoardState CH_BlackFullInitial          = CH_BlackKingInitial |
+                                                     CH_BlackQueenInitial |
+                                                     CH_BlackBishopsInitial |
+                                                     CH_BlackKnightsInitial |
+                                                     CH_BlackRooksInitial |
+                                                     CH_BlackPawnsInitial;
+  const BitBoardState CH_FullInitial               = CH_WhiteFullInitial | CH_BlackFullInitial;
 
 
   class BitBoard
--- test/bitboardtest.cpp
+++ test/bitboardtest.cpp
@@ -1,9 +1,11 @@
+#include <iostream>
 #include <list>
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "bitboard/bitboard.h"
 
+using namespace std;
 using namespace bitboard;
 using namespace testing;
 
@@ -35,7 +37,7 @@ TEST_F(BBTester, B1_Busy)
 TEST_F(BBTester, DiagonalBottomLeftToUpperRightBusy)
 {
     bboard.reset();
-    bboard.setpos(std::list<std::string>
+    bboard.setpos(list<string>
         {"a1", "b2", "c3", "d4", "e5", "f6", "g7", "h8"});
     ASSERT_EQ(bboard.state(), BottomLeftUpperRightDiagonal);
 }
@@ -43,7 +45,7 @@ TEST_F(BBTester, DiagonalBottomLeftToUpperRightBusy)
 TEST_F(BBTester, DiagonalBottomRightToUpperLeftBusy)
 {
     bboard.reset();
-    bboard.setpos(std::list<std::string>
+    bboard.setpos(list<string>
         {"a8", "b7", "c6", "d5", "e4", "f3", "g2", "h1"});
     ASSERT_EQ(bboard.state(), BottomRightUpperLeftDiagonal);
 }
@@ -51,7 +53,7 @@ TEST_F(BBTester, DiagonalBottomRightToUpperLeftBusy)
 TEST_F(BBTester, BoardCenterBusy)
 {
     bboard.reset();
-    bboard.setpos(std::list<std::string>
+    bboard.setpos(list<string>
         {"d4", "e4", "d5", "e5"});
     ASSERT_EQ(bboard.state(), BoardCenter);
 }
@@ -59,14 +61,25 @@ TEST_F(BBTester, BoardCenterBusy)
 TEST_F(BBTester, CornersBusy)
 {
     bboard.reset();
-    bboard.setpos(std::list<std::string>
+    bboard.setpos(list<string>
         {"a1", "h1", "a8", "h8"});
     ASSERT_EQ(bboard.state(), Corners);
 }
 
-TEST(BitBoardTester, UsingConstructor)
+TEST(BitBoardTester, UsingConstructorBothKingsInitial)
 {
-    BitBoard bb(std::list<std::string>{"e1", "e8"});
-    ASSERT_EQ(bb.state(), ChTwoKingsInitial);
+    BitBoard bb(list<string>{"e1", "e8"});
+    ASSERT_EQ(bb.state(), CH_BothKingsInitial);
 
+}
+
+TEST(BitBoardTester, UsingConstructorFullBoardInitial)
+{
+    BitBoard bb(list<string>{
+        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+        "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+        "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+        "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
+    });
+    ASSERT_EQ(bb.state(), CH_FullInitial);
 }
